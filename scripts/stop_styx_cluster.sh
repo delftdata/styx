bfc9d4c0426f911232063b7d142616c3aaa8399d
kPsarakis <kpsarakis94@gmail.com>
2026-02-04 09:07:26 +0100

    cleanup the logfiles and CI

--- scripts/stop_styx_cluster.sh
+++ scripts/stop_styx_cluster.sh
@@ -5,8 +5,9 @@ threads_per_worker=$1
 TS=$(date +"%Y%m%d-%H%M%S")
 
 export STYX_WORKER_THREADS="$threads_per_worker"
-docker compose logs worker | sort -t '|' -k1,1 -k2,2 > "worker-logs-${TS}.log"
-docker compose logs coordinator > "coordinator-logs-${TS}.log"
+mkdir -p logs
+docker compose logs worker | sort -t '|' -k1,1 -k2,2 > "logs/worker-logs-${TS}.log"
+docker compose logs coordinator > "logs/coordinator-logs-${TS}.log"
 
 # DELETE PREVIOUS DEPLOYMENT
 docker compose down --volumes --remove-orphans
